cbb005b217343eeca0315d766b58013ebea69940
flh6 <talk2florencehanson@gmail.com>
2023-02-17 11:23:18 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-#include < stdio.h >
+#include <stdio.h>
+
 /**
- * main - A program that prints an input with puts function
- * Return: 0 (Success)
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
