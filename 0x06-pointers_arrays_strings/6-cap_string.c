b2b616fec09f8e2bf8f088326dfc2af9d177d26e
flh6 <talk2florencehanson@gmail.com>
2023-03-02 04:41:35 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,25 +1,35 @@
-#include <main.h>
-
+#include "main.h"
 /**
- * cap_string - Capitalize all words in a string.
- * @s: The string to be capitalized.
+ *cap_string - capitalizes every first letter of a word in a string.
+ *separators of words are:  space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }.
+ *@s: pointer to string.
  *
- * Return: Pointer to the modified string.
+ *Return: pointer to s.
  */
 char *cap_string(char *s)
 {
-int i;
-for (i = 0; s[i] != '\0'; i++)
+int count;
+/*  scan through string */
+count = 0;
+while (s[count] != '\0')
+{/* if next character after count is a char , capitalise it */
+if (s[0] >= 97 && s[0] <= 122)
 {
-if (i == 0 || isspace(s[i - 1]) ||
-s[i - 1] == ',' || s[i - 1] == ';' ||
-s[i - 1] == '.' || s[i - 1] == '!' ||
-s[i - 1] == '?' || s[i - 1] == '"' ||
-s[i - 1] == '(' || s[i - 1] == ')' ||
-s[i - 1] == '{' || s[i - 1] == '}')
-{
-s[i] = toupper(s[i]);
+s[0] = s[0] - 32;
 }
+if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+|| s[count] == '.' || s[count] == '!' || s[count] == '?'
+|| s[count] == '"' || s[count] == '(' || s[count] == ')'
+|| s[count] == '{' || s[count] == '}')
+		{
+if (s[count + 1] >= 97 && s[count + 1] <= 122)
+{
+s[count + 1] = s[count + 1] - 32;
+}
+}
+count++;
 }
 return (s);
 }
