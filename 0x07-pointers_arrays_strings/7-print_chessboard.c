36df3d2e3c8644cc8fe35f8579dee7e2a323daa6
flh6 <talk2florencehanson@gmail.com>
2023-03-06 14:02:34 -0800

    Added

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,50 +1,17 @@
 #include "main.h"
-
-
 /**
- *  * print_chessboard - prints the chessboard
- *   * @a: the 2D array representing the chessboard
- *    *
- *     * Return: void
+ * *   print_chessboard - print chessboard given set 2D array
+ * *    @a: 2D array
 */
-void print_chessboard(char (*a)[8]);
 
-/**
- *  * main - check the print_chessboard function
- *   *
- *    * Return: Always 0.
-*/
-int main(void)
-{
-char board[8][8] = {
-{'r', 'k', 'b', 'q', 'k', 'b', 'k', 'r'},
-{'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-{'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
-{'R', 'K', 'B', 'Q', 'K', 'B', 'K', 'R'},
-};
-print_chessboard(board);
-return (0);
-}
-
-/**
- *  * print_chessboard - prints the chessboard
- *   * @a: the 2D array representing the chessboard
- *    *
- *     * Return: void
-*/
 void print_chessboard(char (*a)[8])
 {
-int i, j;
-for (i = 0; i < 8; i++)
+int row;
+int column;
+for (row = 0; row < 8; row++)
 {
-for (j = 0; j < 8; j++)
-}
-putchar(a[i][j]);
-}
-putchar('\n');
+for (column = 0; column < 8; column++)
+_putchar(a[row][column]);
+_putchar('\n');
 }
 }
