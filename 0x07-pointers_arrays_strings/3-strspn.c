c58e18cfc72ee6ebf9b0b66cacd5c27def12a2a1
flh6 <talk2florencehanson@gmail.com>
2023-03-06 14:35:00 -0800

    Added

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,27 +1,28 @@
 #include "main.h"
 /**
- *  * _strspn - gets the length of a prefix substring.
- *   * @s: pointer to a string to be searched
- *    * @accept: pointer to a string containing characters to match
- *     *
- *      * Return: the number of bytes in the initial segment of s which consist only of bytes from accept
+ * *   _strspn - return length of string that matches values consistently
+ * *    @s: string to search
+ * *     @accept: target matches
+ * *      Return: number of bytes consecutively matched
 */
+
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int i, j, count = 0;
-
-for (i = 0; s[i]; i++)
+int i = 0, j;
+int matches = 0;
+while (s[i] != '\0') /*iterate through string*/
 {
-for (j = 0; accept[j]; j++)
+for (j = 0; accept[j] != '\0'; j++) /*iterate through target*/
 {
-if (s[i] == accept[j])
+if (s[i] == accept[j]) /*record & break at first match*/
 {
-count++;
+matches++;
 break;
 }
+if (accept[j + 1] == '\0' && s[i] != accept[j])
+return (matches);/*return if idx doesn't match*/
 }
-if (!accept[j])
-break;
+i++;
 }
-turn count;
+return (matches); /* return num if all match till end */
 }
