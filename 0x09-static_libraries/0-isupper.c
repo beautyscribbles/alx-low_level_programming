86eec25f5bdaec19bd38dab99cacfffefdc601bf
Florence <118609247+florencelhanson@users.noreply.github.com>
2023-03-10 19:40:11 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,14 +1,15 @@
-#include "main.h"
 /**
- * _isupper - uppercase letters
- * @c: char to check
+ * _isupper - check if c is upper
  *
- * Return: 0 or 1
- */
+ * @c: input for alphabet
+ *
+ * Return: 1 if its uppercase, 0 if not
+*/
+
 int _isupper(int c)
 {
-if (c >= 'A' && c <= 'Z')
-return (1);
-else
-return (0);
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
 }
