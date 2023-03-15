64887b402991683ddc33af24e9787eafa95dae5c
flh6 <talk2florencehanson@gmail.com>
2023-03-15 13:23:36 -0700

    Updated

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,32 +1,33 @@
-#include "mainh"
+#include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
- * alloc_grid - creates integers of 2 dimensional array
- * @width: list the row of the array
- * @height: list the column of the array
- * Return: points to a 2 dimensional array
- */
+ * alloc_grid -  Entry point
+ * @width: input
+ * @height: input
+ * Return: 0
+*/
 int **alloc_grid(int width, int height)
 {
-int **arr;
+int **grid;
 int i, j;
 if (width <= 0 || height <= 0)
 return (NULL);
-arr = malloc(height * sizeof(int *));
-if (arr == NULL)
+grid = malloc(height * sizeof(int *));
+if (grid == NULL)
 return (NULL);
-arr[0] = malloc((width * height) * sizeof(int));
-if (arr[0] == NULL)
+for (i = 0; i < height; i++)
 {
-free(arr);
+grid[i] = malloc(width * sizeof(int));
+if (grid[i] == NULL)
+{
+while (i >= 0)
+free(grid[i--]);
+free(grid);
 return (NULL);
 }
-for (i = 0; i < height; i++)
-arr[i] = arr[i - 1] + width;
-for (i = 0; i < height; i++)
-{
 for (j = 0; j < width; j++)
-arr[i][j] = 0;
+grid[i][j] = 0;
 }
-return (arr);
+return (grid);
 }
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,13 +4,13 @@
  * free_grid - frees a 2 dimensional grid
  * @grid: grid that will be fred
  * @height: height of grid to be fred
- * Return: freed grid
+ * Return: 0
  */
 void free_grid(int **grid, int height)
 {
 int i;
 if (grid == NULL)
-return;
+return (0);
 for (i = 0; i < height; i++)
 free(grid[i]);
 free(grid);
