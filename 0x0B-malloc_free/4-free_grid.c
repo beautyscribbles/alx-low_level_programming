616a2a7d992d0373cc8f9a47c906edaf435a3893
flh6 <talk2florencehanson@gmail.com>
2023-03-15 13:29:52 -0700

    Updated

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,17 +1,20 @@
-#include "mAin.h"
+#include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
- * free_grid - frees a 2 dimensional grid
- * @grid: grid that will be fred
- * @height: height of grid to be fred
- * Return: 0
+ * free_grid - Main Entry
+ * @grid: input
+ * @height: input
  */
 void free_grid(int **grid, int height)
 {
 int i;
-if (grid == NULL)
-return (0);
+if (grid != NULL || height != 0)
+{
 for (i = 0; i < height; i++)
+{
 free(grid[i]);
+}
 free(grid);
+}
 }
