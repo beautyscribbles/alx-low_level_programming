d9b5004d3708b40e3309648d6898088c8b4d2bdd
flh6 <talk2florencehanson@gmail.com>
2023-03-16 07:50:04 -0700

    Updated

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  * malloc_checked - Main Entry
+ *   * @b: input
+ *    * Return: 0
+*/
+void *malloc_checked(unsigned int b)
+{
+void *p;
+p = malloc(b);
+if (p == NULL)
+exit(98);
+return (p);
+}
