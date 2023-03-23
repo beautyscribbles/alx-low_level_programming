eb4b25ed6ca7ae25af12884fe8312f56d7f4491d
flh6 <talk2florencehanson@gmail.com>
2023-03-23 00:02:16 -0700

    Updated

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
-#ifndef FUNC_H
-#define FUNC_H
-#include <stdlib.h>
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
 
+#include <stddef.h>
+
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
