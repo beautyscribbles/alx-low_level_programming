cef8f23cd53b8cd98c6ddbbeca996c6c87f585a3
flh6 <talk2florencehanson@gmail.com>
2023-04-05 14:04:32 -0700

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,12 @@
+/*Beauty Scribbles*/
 #include "lists.h"
 
-/*
- * print_listint - prints all the elements of a listint_t list.
- * @h: Input
- * Return: number of nodes.
-**/
-size_t print_listint(const listint_t *h)
+ /**
+  *  * print_listint - prints all the elements of a listint_t list.
+  *   * @h: input
+  *    * Return: number of nodes.
+*/
+/*Protype used*/ size_t print_listint(const listint_t *h)
 {
 const listint_t *node = h;
 size_t arrow = 0;
@@ -15,5 +16,6 @@ printf("%i\n", node->n);
 arrow++;
 node = node->next;
 }
-return (arrow);
+return (arrow);/*return statement*/
 }
+
