ce112c29511a820aad65fd5949e15746958a1ab9
flh6 <talk2florencehanson@gmail.com>
2023-04-08 07:53:49 -0700

    Updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,44 +1,32 @@
 #include "main.h"
-
 /**
- * binary_to_uint - Entry point
- * Description: Converts a binary number to an unsigned int
- * @b: Pointer to a string of 0 and 1 chars
- *
- * Return: The converted number, or 0 if b is NULL or there's a non 0 or 1 char
- */
-
+ *  *binary_to_uint - converts a binary number to an unsigned int
+ *   *@b: Input
+ *    *Return: 1 when succesful
+*/
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int digit = 0;
-	unsigned int i = 0;
-
-	/* Check if b is NULL */
-	if (!b)
-	{
-		return (0);
-	}
-	/* Loop: gets the length of the binary string */
-	while (b[i] != '\0')
-	{
-		/* Checks if char is not 0 or 1 */
-		if (b[i] != '0' && b[i] != '1')
-		{
-			return (0);
-		}
-		i++;
-	}
-	/* Get the decimal value of the binary string */
-	i = 0;
-	while (b[i] != '\0')
-	{
-		/* Left shift the digit by 1 bit or space */
-		digit <<= 1;
-		if (b[i] == '1')
-		{
-			digit += 1;
-		}
-		i++;
-	}
-	return (digit);
+int i = 0;
+unsigned int output = 0;
+if (b == NULL)
+{
+return (0);
+}
+while (b[i])
+{
+if (b[i] == '0')
+{
+output =  ((2 * output)+(b[i] - '0'));
+}
+else if (b[i] == '1')
+{
+output =  ((2 * output)+(b[i] - '0'));
+}
+else
+{
+return (0);
+}
+i++;
+}
+return (output);
 }
