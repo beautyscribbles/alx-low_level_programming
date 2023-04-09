9c3b839bccdb51c4245713dab2e971251f1c726a
flh6 <talk2florencehanson@gmail.com>
2023-04-08 23:14:14 -0700

    Appended a text to a c file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,7 +20,10 @@ if (fd == -1)
 return (-1);
 if (text_content)
 {
-for (nletters = 0; text_content[nletters]; nletters++);
+for
+(nletters = 0;
+text_content[nletters];
+nletters++);
 rwr = write(fd, text_content, nletters);
 if (rwr == -1)
 return (-1);
