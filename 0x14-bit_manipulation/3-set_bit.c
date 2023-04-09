45d789faba14e480eb5e1fc8f9a07bafd1748caa
flh6 <talk2florencehanson@gmail.com>
2023-04-09 09:41:44 -0700

    added input to set bit file

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - Entry point
- * Description: Sets the value of a bit to 1
- * @n: Changes number
- * @index: the number to be set
- * Return: 1 if successful or -1 if unsuccessful
+ *  * set_bit - a function that sets the value of a bit to 1 at a given index.
+ *   * @n: input
+ *    * @index: input
+ *     *
+ *      * Return: 1 if success, or -1 if unsuccessful
 */
+
 int set_bit(unsigned long int *n, unsigned int index)
-/* The index is within the range of -8 */
-if (index >= (sizeof(unsigned long int) * 8))
 {
+if (index > 63)
 return (-1);
-}
-/*  The bit is set to 1 */
-*n |= (1 << index);
+*n = ((1UL << index) | *n);
 return (1);
 }
