854b81e694553adfbbc8ad4be00b35254c6bd9ab
flh6 <talk2florencehanson@gmail.com>
2023-05-08 21:18:33 -0700

    created a new directory 0x13-file_io

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,15 @@
+#ifndef FILE_IO /*MAIN_H*/
+#define FILE_IO /*MAIN_H*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+int main(int argc, char *argv[]);
+void error_checks(int file_to, int file_from, char *argv[]);
+#endif /*MAIN_H*/
