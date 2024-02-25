098bf34582e17d0435782dc34c2b74dd48731c17
Florence Hanson <118609247+beautyscribbles@users.noreply.github.com>
2024-02-25 06:10:41 +0100

    Updated lists.h

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,27 +2,29 @@
 #define LISTS_H
 
 /*
- *  * File: lists.h
- *   * Auth: Florence Hanson
- *     * Desc: Header file containing prototypes and definitions     */
+ * File: lists.h
+ * Auth: Florence Hanson
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x11-singly_linked_lists directory.
+ */
 
 #include <stdlib.h>
 
 /**
- *  * struct list_s - singly linked list
- *   * @str: string - (malloc'ed string)
- *    * @len: length of the string
- *     * @next: points to the next node
- *      *
- *       * Description: singly linked list node structure
-*/       
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *              for Holberton project
+ */
 typedef struct list_s
 {
-char *str;
-unsigned int len;
-struct list_s *next;
-}
-list_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
