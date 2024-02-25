3c1ba31198b6b2bb7a51a91f50aa42f4333de3ff
Florence Hanson <118609247+beautyscribbles@users.noreply.github.com>
2024-02-25 07:19:17 +0100

    Updated 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,34 +1,41 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
-
 /**
- *  * str_concat - Concatenates two strings.
- *   * @s1: The first string to be concatenated.
- *    * @s2: The second string to be concatenated.
- *     *
- *      * Return: If concatenation fails - NULL.
- *       *         Otherwise - a pointer to the newly-allocated space in memory
- *        *                     containing the concatenated strings.
-*/
+ * str_concat - Main Entry
+ * @s1: input
+ * @s2: input
+ * Return: 0
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *concat_str;
-int i, j, len1 = 0, len2 = 0;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-while (s1[len1] != '\0')
-len1++;
-while (s2[len2] != '\0')
-len2++;
-concat_str = malloc(sizeof(char) * (len1 + len2 + 1));
-if (concat_str == NULL)
-return (NULL);
-for (i = 0; s1[i] != '\0'; i++)
-concat_str[i] = s1[i];
-for (j = 0; s2[j] != '\0'; j++)
-concat_str[i + j] = s2[j];
-concat_str[i + j] = '\0';
-return (concat_str);
+	unsigned int size1 = 0, size2 = 0;
+	char *ptr, *ret;
+
+	ptr = s1;
+	if (s1)
+		while (*ptr++)
+			size1++;
+	else
+		s1 = "";
+
+	ptr = s2;
+	if (s2)
+		while (*ptr++)
+			size2++;
+	else
+		s2 = "";
+
+	ret = malloc(size1 + size2 + 1);
+	if (!ret)
+		return (NULL);
+
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	while (*s2)
+		*ptr++ = *s2++;
+	*ptr = 0;
+
+	return (ret);
 }
