12274d56e2b5cf0387434e3f70e949d70b0f372e
Florence Hanson <118609247+beautyscribbles@users.noreply.github.com>
2024-02-25 07:22:37 +0100

    Updated 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -8,13 +8,14 @@
  */
 void free_grid(int **grid, int height)
 {
-int i;
-if (grid != NULL || height != 0)
-{
-for (i = 0; i < height; i++)
-{
-free(grid[i]);
-}
-free(grid);
-}
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
 }
